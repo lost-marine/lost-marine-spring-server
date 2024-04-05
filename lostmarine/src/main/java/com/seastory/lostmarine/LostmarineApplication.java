39b3736f315ca9eb0d4fbcca9745c83f2bf99721
양소영 <maysoyoung@gmail.com>
2024-04-05 14:00:49 +0900

    Chore: 프로젝트 초기 설정

---
+++ lostmarine/src/main/java/com/seastory/lostmarine/LostmarineApplication.java
@@ -0,0 +1,13 @@
+package com.seastory.lostmarine;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LostmarineApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LostmarineApplication.class, args);
+	}
+
+}
---
+++ lostmarine/src/test/java/com/seastory/lostmarine/LostmarineApplicationTests.java
@@ -0,0 +1,13 @@
+package com.seastory.lostmarine;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LostmarineApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
